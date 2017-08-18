82fa808c9c86185f7b73629fd5774f283929d146
lribelle@tbxnet.com <lribelle@tbxnet.com>
2017-08-18 15:31:30 -0300

    Added files v0.0.1

---
+++ README.md
@@ -0,0 +1,67 @@
+# videojs-tracking-events
+
+event tracker
+
+## Table of Contents
+
+<!-- START doctoc -->
+<!-- END doctoc -->
+## Installation
+
+```sh
+npm install --save videojs-tracking-events
+```
+
+## Usage
+
+To include videojs-tracking-events on your website or web application, use any of the following methods.
+
+### `<script>` Tag
+
+This is the simplest case. Get the script in whatever way you prefer and include the plugin _after_ you include [video.js][videojs], so that the `videojs` global is available.
+
+```html
+<script src="//path/to/video.min.js"></script>
+<script src="//path/to/videojs-tracking-events.min.js"></script>
+<script>
+  var player = videojs('my-video');
+
+  player.trackingEvents();
+</script>
+```
+
+### Browserify/CommonJS
+
+When using with Browserify, install videojs-tracking-events via npm and `require` the plugin as you would any other module.
+
+```js
+var videojs = require('video.js');
+
+// The actual plugin function is exported by this module, but it is also
+// attached to the `Player.prototype`; so, there is no need to assign it
+// to a variable.
+require('videojs-tracking-events');
+
+var player = videojs('my-video');
+
+player.trackingEvents();
+```
+
+### RequireJS/AMD
+
+When using with RequireJS (or another AMD library), get the script in whatever way you prefer and `require` the plugin as you normally would:
+
+```js
+require(['video.js', 'videojs-tracking-events'], function(videojs) {
+  var player = videojs('my-video');
+
+  player.trackingEvents();
+});
+```
+
+## License
+
+UNLICENSED. Copyright (c) lribelle@tbxnet.com &lt;lribelle@tbxnet.com&gt;
+
+
+[videojs]: http://videojs.com/
---
+++ src/plugin.js
@@ -0,0 +1,190 @@
+import videojs from 'video.js';
+import {version as VERSION} from '../package.json';
+import { percentage as PERCENTAGE, events as EVENTS } from './events.json';
+
+// Default options for the plugin.
+const defaults = {
+  url: 'http://localhost:9999',
+  contentId: 'content1234',
+  profileId: 'prof1234',
+  request: {
+    headers: {}
+  }
+};
+
+// Cross-compatibility for Video.js 5 and 6.
+const registerPlugin = videojs.registerPlugin || videojs.plugin;
+// const dom = videojs.dom || videojs;
+
+// Saves last time
+let lastTime = 0;
+// Playback Start Date
+let startDate = null;
+// Array with events already sent
+let eventsSent = [];
+
+/**
+ * Function to invoke when the player is ready.
+ *
+ * @function onPlayerReady
+ * @param    {Player} player
+ *           A Video.js player object.
+ *
+ * @param    {Object} [options={}]
+ *           A plain object containing options for the plugin.
+ */
+const onPlayerReady = (player, options) => {
+  player.addClass('vjs-tracking-events');
+};
+
+/**
+ * Function to invoke when the event pause is triggered.
+ * 
+ * @param {videojs} player
+ * @param {Object} options
+ */
+const onPauseEvent = (player, options) => {
+  sendEvent(EVENTS.PAUSE, player, options);
+};
+
+/**
+ * Function to invoke when the event play is triggered.
+ * 
+ * @param {videojs} player
+ * @param {Object} options
+ */
+const onResumeEvent = (player, options) => {
+  sendEvent(EVENTS.RESUME, player, options);
+};
+
+/**
+ * Function to invoke when the event timeupdate is triggered.
+ * 
+ * @param {videojs} player
+ * @param {Object} options 
+ */
+const onTimeUpdate = (player, options) => {
+  const percentage = Math.round((player.currentTime() / player.duration()) * 100);
+
+  if (lastTime > player.currentTime()) {
+    // Clean quartile events sent    
+    cleanEventsSent(percentage);
+  }
+
+  // Check quartiles
+  const event = getQuartileEvent(percentage);
+
+  if (event) {
+    // Add event to array of events sent
+    eventsSent.push(event);
+    sendEvent(event, player, options);
+  }
+  
+  // Set lastTime with currentTime for quartile reset
+  lastTime = player.currentTime();
+};
+
+/**
+ * Sends event with player data
+ * 
+ * @param {String} event 
+ * @param {videojs} player 
+ */
+function sendEvent(event, player, options) {
+  const playerData = {
+    position: player.currentTime(),
+    timeSpent: Math.round((Date.now() - startDate) / 1000),
+    event
+  };
+
+  console.log(playerData);
+}
+
+/**
+ * Function to send request through videojs xhr.
+ * 
+ * @param {String} url 
+ * @param {Object} data 
+ */
+function makeRequest(data) {
+  let request = {
+    body: JSON.stringify(data),
+    url: eventUrl,
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  }
+
+  videojs.xhr(request, (err, res, body) => {
+    console.log('res');
+  });
+}
+
+/**
+ * Returns quartile event if any
+ * 
+ * @param {any} percentage 
+ */
+function getQuartileEvent(percentage) {
+  if (percentage >= PERCENTAGE.FIRSTQUARTILE && !eventsSent.includes(EVENTS.FIRSTQUARTILE)) {
+    return EVENTS.FIRSTQUARTILE;
+  } else if (percentage >= PERCENTAGE.MIDPOINT && !eventsSent.includes(EVENTS.MIDPOINT)) {
+    return EVENTS.MIDPOINT;
+  } else if (percentage >= PERCENTAGE.THIRDQUARTILE && !eventsSent.includes(EVENTS.THIRDQUARTILE)) {
+    return EVENTS.THIRDQUARTILE;
+  } else if (percentage >= PERCENTAGE.COMPLETE && !eventsSent.includes(EVENTS.COMPLETE)) {
+    return EVENTS.COMPLETE;
+  }
+  return null;
+}
+
+/**
+ * Clears events on rewind
+ * 
+ * @param {integer} percentage 
+ */
+function cleanEventsSent(percentage) {
+  eventsSent = eventsSent.filter(event => {
+    return percentage > PERCENTAGE[event.toUpperCase()];
+  });
+}
+
+/**
+ * Hooks player events
+ *
+ * @param {videojs} player
+ * @param {Object} options
+ */
+const hookPlayerEvents = (player, options) => {
+  player.on('timeupdate', onTimeUpdate.bind(null, player, options));
+  player.on('pause', onPauseEvent.bind(null, player, options));
+  player.on('play', onResumeEvent.bind(null, player, options));
+};
+
+/**
+ * A video.js plugin.
+ *
+ * In the plugin function, the value of `this` is a video.js `Player`
+ * instance. You cannot rely on the player being in a "ready" state here,
+ * depending on how the plugin is invoked. This may or may not be important
+ * to you; if not, remove the wait for "ready"!
+ *
+ * @function trackingEvents
+ * @param    {Object} [options={}]
+ *           An object of options left to the plugin author to define.
+ */
+const trackingEvents = function(options) {
+  this.ready(() => {
+    startDate = Date.now();
+    hookPlayerEvents(this, videojs.mergeOptions(defaults, options));
+  });
+};
+
+// Register the plugin with video.js.
+registerPlugin('trackingEvents', trackingEvents);
+
+// Include the version number.
+trackingEvents.VERSION = VERSION;
+
+export default trackingEvents;
